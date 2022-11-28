bf496c5c07398bb5bc3f4ae0c2b024f4b91a38d8
mike buss <mikebuss@live.ca>
2022-11-27 20:12:13 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css';
+import React from "react";
+import Main from './components/MainPage'
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-black grid place-items-center">
+<Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,5 @@
+export default function Main () {
+return (
+<div className="font-bold text-gray-500">Semisquircle</div>
+);
+}
